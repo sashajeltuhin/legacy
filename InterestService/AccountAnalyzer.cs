849499baf4294e6d0b16f5bf39b4020a47e96ed3
Sasha Jeltuhin <sjeltuhin@gmail.com>
2017-08-15 23:21:23 -0400

    Initial revision

---
+++ InterestService/AccountAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Apprenda.Services.Logging;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace InterestService
+{
+  
+    public class AccountAnalyzer : IAccountAnalyzer
+    {
+        private static readonly ILogger Log = LogManager.Instance().GetLogger(typeof(AccountAnalyzer));
+
+        public IEnumerable<MonthlyInterest> GetAccountData(string accountID, string accountBalance)
+        {
+            
+            double[] arr = new double[12] { 0.01, 0.013, 0.013, 0.013, 0.013, 0.011, 0.013, 0.02, 0.01, 0.0129, 0.0131, 0.011 };
+            List<MonthlyInterest> list = new List<MonthlyInterest>();
+            try
+            {
+                double balance = double.Parse(accountBalance);
+                double cum = balance;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    MonthlyInterest mi = new MonthlyInterest();
+                    mi.Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i + 1);
+                    mi.Rate = arr[i];
+                    cum += cum * arr[i];
+                    mi.Total = cum;
+                    list.Add(mi);
+                }
+                Log.Log(string.Format("Account {0} successfully processed", accountID), LogLevel.Info);
+            }
+            catch(Exception ex)
+            {
+                Log.Log(string.Format("Error analyzing the account {0}. {1}", accountID, ex.Message), LogLevel.Error);
+            }
+            return list;
+        }
+    }
+}
---
+++ InterestService/IAccountAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace InterestService
+{
+    [ServiceContract]
+    public interface IAccountAnalyzer
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat =WebMessageFormat.Json,
+            UriTemplate = "GetAccountData/{accountID}/{accountBalance}")]
+        IEnumerable<MonthlyInterest> GetAccountData(string accountID, string accountBalance);
+
+ 
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "InterestService.ContractType".
+    [DataContract]
+    public class MonthlyInterest
+    {
+        double total = 0;
+        double rate = 0;
+        string month = string.Empty;
+
+        [DataMember]
+        public string Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+
+        [DataMember]
+        public double Total { get => total; set => total = value; }
+
+        [DataMember]
+        public double Rate { get => rate; set => rate = value; }
+    }
+}
