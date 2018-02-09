2473a03eb524326208738188e007a7e5ca597d04
Sasha Jeltuhin <sjeltuhin@gmail.com>
2018-02-09 18:39:19 -0500

    DB support

--- InterestService/AccountAnalyzer.cs
+++ InterestService/AccountAnalyzer.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Data.Common;
+using System.Configuration;
 
-namespace InterestService
+namespace AccountService
 {
   
     public class AccountAnalyzer : IAccountAnalyzer
@@ -18,7 +20,7 @@ namespace InterestService
         public IEnumerable<MonthlyInterest> GetAccountData(string accountID, string accountBalance)
         {
             
-            double[] arr = new double[12] { 0.01, 0.013, 0.013, 0.013, 0.013, 0.011, 0.013, 0.02, 0.01, 0.0129, 0.0131, 0.011 };
+            double[] arr = LoadData(3); //new double[12] { 0.01, 0.013, 0.013, 0.013, 0.013, 0.011, 0.013, 0.02, 0.01, 0.0129, 0.0131, 0.011 };
             List<MonthlyInterest> list = new List<MonthlyInterest>();
             try
             {
@@ -45,8 +47,8 @@ namespace InterestService
         public IEnumerable<ClaimsData> GetClaimsData(string accountID)
         {
 
-            double[] totalClaim = new double[12] { 350, 245, 70, 0, 0, 156, 0, 876, 0, 240, 432, 0 };
-            double[] pocket = new double[12] { 80, 190, 70, 0, 0, 25, 0, 453, 0, 45, 187, 0 };
+            double[] totalClaim = LoadData(1);//new double[12] { 350, 245, 70, 0, 0, 156, 0, 876, 0, 240, 432, 0 };
+            double[] pocket = LoadData(2);//new double[12] { 80, 190, 70, 0, 0, 25, 0, 453, 0, 45, 187, 0 };
             List<ClaimsData> list = new List<ClaimsData>();
             try
             {
@@ -65,6 +67,37 @@ namespace InterestService
                 Log.Log(string.Format("Error getting claims for the account {0}. {1}", accountID, ex.Message), LogLevel.Error);
             }
             return list;
+        }
+
+        private double [] LoadData(int type)
+        {
+            double[] data = new double[12];
+            string[] fields = new string[12] {"[jan]", "[feb]", "[mar]", "[apr]", "[may]", "[jun]", "[jul]", "[aug]", "[sep]", "[oct]", "[nov]", "[dec]" };
+            DbProviderFactory providerFactory =
+           DbProviderFactories.GetFactory(ConfigurationManager
+           .ConnectionStrings["legacy"].ProviderName);
+            using (DbConnection conn = providerFactory.CreateConnection())
+            {
+                conn.ConnectionString = ConfigurationManager
+                .ConnectionStrings["legacy"]
+               .ConnectionString;
+                DbCommand command = conn.CreateCommand();
+                command.CommandText =
+               "select " + string.Join(",", fields) + $" from [AccountData] where [tupleType] = {type}";
+                Log.Log(string.Format("Executing query with connection string {0}. {1}", command.CommandText), LogLevel.Info);
+                conn.Open();
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for(int i = 0; i < 12; i++)
+                        {
+                            data[i] = (double)reader[fields[i]];
+                        }
+                    }
+                }
+            }
+            return data;
         }
     }
 }
--- InterestService/IAccountAnalyzer.cs
+++ InterestService/IAccountAnalyzer.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace InterestService
+namespace AccountService
 {
     [ServiceContract]
     public interface IAccountAnalyzer
